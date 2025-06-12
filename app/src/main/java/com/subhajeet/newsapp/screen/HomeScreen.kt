6020a43ab3c71ecfb24aef8afb5e0d1cc06c1bbc
SubhajeetBiswas-2021 <subhajeet.bs2021@gmail.com>
2025-06-12 21:03:35 +0530

    commit

--- app/src/main/java/com/subhajeet/newsapp/screen/HomeScreen.kt
+++ app/src/main/java/com/subhajeet/newsapp/screen/HomeScreen.kt
@@ -1,11 +1,21 @@
 package com.subhajeet.newsapp.screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
 import com.subhajeet.newsapp.viewModels.NewsViewModels
+
 
 
 @Composable
@@ -19,7 +29,32 @@ fun HomeScreen(viewModels : NewsViewModels){
         LazyColumn {
             items(res){
 
-                Text(text=it.title)
+                eachCard(title = it.title , imageUrl= it.urlToImage)
+            }
+
+        }
+    }
+}
+
+@Composable
+fun eachCard(
+    title:String,
+    imageUrl:String
+){
+
+    Card(modifier = Modifier.fillMaxWidth().padding(8.dp))
+    {
+        Row(){
+
+            AsyncImage(
+                model = "imageUrl",
+                contentDescription = null,
+            )
+            Column {
+                Text(
+                    text = "title",
+                    fontWeight = FontWeight.Bold,
+                )
             }
 
         }
