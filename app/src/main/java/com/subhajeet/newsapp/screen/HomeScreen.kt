2396b8c5285f134ca45fa5f48157d5e880a00710
SubhajeetBiswas-2021 <subhajeet.bs2021@gmail.com>
2025-06-13 11:42:56 +0530

    commit

--- app/src/main/java/com/subhajeet/newsapp/screen/HomeScreen.kt
+++ app/src/main/java/com/subhajeet/newsapp/screen/HomeScreen.kt
@@ -6,16 +6,24 @@ package com.subhajeet.newsapp.screen
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Face
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
@@ -42,18 +50,23 @@ fun HomeScreen(viewModels : NewsViewModels){
 
 @Composable
 fun eachCard(
-    title:String,
-    imageUrl:String
+    title:String?,
+    imageUrl:String?,
 ){
 
     Card(modifier = Modifier.fillMaxWidth().padding(8.dp))
     {
-        Row(){
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth().height(100.dp).padding(8.dp)
+        ){
 
             if(imageUrl != null){
                 AsyncImage(
-                    model = "imageUrl",
+                    model = imageUrl,
                     contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.size(80.dp).clip(RoundedCornerShape(8.dp))
                 )
             }else{
                 Image(
@@ -63,12 +76,21 @@ fun eachCard(
 
             }
 
+            Spacer(modifier = Modifier.width(8.dp))
 
-            Column {
-                Text(
-                    text = "title",
-                    fontWeight = FontWeight.Bold,
-                )
+
+            Column{
+
+
+                if(title != null){
+                    Text(
+                        text = title,
+                        fontWeight = FontWeight.Bold,
+                    )
+                }else{
+                    Text(text = "Title is not found")
+                }
+
             }
 
         }
